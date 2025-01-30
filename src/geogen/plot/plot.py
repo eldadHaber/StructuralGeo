642981d11862669837919fd8a33799f5f59d1d3d
Simon Ghyselincks <sghyselincks@gmail.com>
2025-01-29 18:53:48 -0800

    updated discrete plotting

--- src/geogen/plot/plot.py
+++ src/geogen/plot/plot.py
@@ -9,12 +9,18 @@ import pyvista as pv
 import matplotlib.pyplot as plt
 
 from geogen.model import GeoModel
-from geogen.generation import BED_ROCK_VAL, SEDIMENT_VALS, DIKE_VALS, INTRUSION_VALS, BLOB_VALS
+from geogen.generation import (
+    BED_ROCK_VAL,
+    SEDIMENT_VALS,
+    DIKE_VALS,
+    INTRUSION_VALS,
+    BLOB_VALS,
+)
 
 
 def get_plot_config():
-    color_range = (-1,13)
-    clim = (color_range[0]-0.5, color_range[1]+0.5)
+    color_range = (-1, 13)
+    clim = (color_range[0] - 0.5, color_range[1] + 0.5)
     n_colors = color_range[1] - color_range[0] + 1
     my_cmap = plt.get_cmap("gist_ncar", n_colors)
 
@@ -33,11 +39,10 @@ def get_plot_config():
 
     for val in BLOB_VALS:
         annotations[float(val)] = "Minerals"
-    
-    
+
     plot_config = {
         "cmap": my_cmap,
-        # Shift so each integer is the center of a bin: 
+        # Shift so each integer is the center of a bin:
         "clim": clim,
         "n_colors": n_colors,
         # Provide annotation dict for -1..15
@@ -54,6 +59,7 @@ def get_plot_config():
         },
     }
     return plot_config
+
 
 def setup_plot(model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=-0.5):
     if plotter is None:
@@ -102,9 +108,11 @@ def volview(
     if clim:
         plot_config["clim"] = clim
 
-    plotter.add_mesh(mesh, scalars="values", **plot_config, interpolate_before_map=False)
+    plotter.add_mesh(
+        mesh, scalars="values", **plot_config, interpolate_before_map=False
+    )
     plotter.add_axes(line_width=5)
-    
+
     flat_bounds = [item for sublist in model.bounds for item in sublist]
 
     if show_bounds:
@@ -128,7 +136,9 @@ def volview(
     return plotter
 
 
-def orthsliceview(model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=-0.5) -> pv.Plotter:
+def orthsliceview(
+    model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=-0.5
+) -> pv.Plotter:
     """
     Visualize using interactive orthogonal slices of the geological model.
 
@@ -189,7 +199,9 @@ def nsliceview(
     return plotter
 
 
-def onesliceview(model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=-0.5) -> pv.Plotter:
+def onesliceview(
+    model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=-0.5
+) -> pv.Plotter:
     """
     Visualize a single slice through the geological model, along with a translucent surface for context.
 
@@ -224,7 +236,9 @@ def onesliceview(model: GeoModel, plotter: Optional[pv.Plotter] = None, threshol
     return plotter
 
 
-def transformationview(model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=None) -> pv.Plotter:
+def transformationview(
+    model: GeoModel, plotter: Optional[pv.Plotter] = None, threshold=None
+) -> pv.Plotter:
     """
     Visualize a time-sequenced transformation view of the geological model, showing snapshots of model deformations.
 
@@ -285,14 +299,18 @@ def _add_snapshots_to_plotter(plotter: pv.Plotter, model: GeoModel, cmap, clim):
     x_offset = model.bounds[0][1] - model.bounds[0][0]  # Width of the model along x
 
     # Remove first data time entry which is empty, add the final data time entry
-    data_snapshots = np.concatenate((model.data_snapshots[1:], model.data.reshape(1, -1)), axis=0)
+    data_snapshots = np.concatenate(
+        (model.data_snapshots[1:], model.data.reshape(1, -1)), axis=0
+    )
 
     # Reverse the snapshots for proper plotting
     mesh_snapshots = model.mesh_snapshots[::-1]
     data_snapshots = data_snapshots[::-1]
 
     actors = []
-    for i, (mesh_snapshot, data_snapshot) in enumerate(zip(mesh_snapshots, data_snapshots)):
+    for i, (mesh_snapshot, data_snapshot) in enumerate(
+        zip(mesh_snapshots, data_snapshots)
+    ):
         # Assuming snapshots are stored as Nx3 arrays
         # Reshape to 3D grid of points-- i.e. 4x4x4 grid of (x,y,z) points
         deformed_points = mesh_snapshot.reshape(resolution + (3,))
@@ -320,7 +338,9 @@ def _add_snapshots_to_plotter(plotter: pv.Plotter, model: GeoModel, cmap, clim):
     return actors
 
 
-def categorical_grid_view(model: GeoModel, threshold=None, text_annot=True, off_screen=False) -> pv.Plotter:
+def categorical_grid_view(
+    model: GeoModel, threshold=None, text_annot=True, off_screen=False
+) -> pv.Plotter:
     """
     Visualize categorical rock types from the geological model in a grid layout, with each category displayed separately.
 
@@ -355,7 +375,9 @@ def categorical_grid_view(model: GeoModel, threshold=None, text_annot=True, off_
     num_cats = len(cats)
     rows, cols = calculate_grid_dims(num_cats)
 
-    p = pv.Plotter(shape=(rows, cols), border=False, off_screen=off_screen)  # subplot square layout
+    p = pv.Plotter(
+        shape=(rows, cols), border=False, off_screen=off_screen
+    )  # subplot square layout
 
     clim = [cats.min(), cats.max()]  # Preset color limits for all subplots
     skin = grid.extract_surface()  # Extract surface mesh for translucent skin
@@ -365,7 +387,9 @@ def categorical_grid_view(model: GeoModel, threshold=None, text_annot=True, off_
         p.subplot(row, col)
 
         cat_mask = grid["values"] == cat  # mask for a category
-        category_grid = grid.extract_cells(cat_mask)  # Pull only those cells from voxel grid
+        category_grid = grid.extract_cells(
+            cat_mask
+        )  # Pull only those cells from voxel grid
 
         # Plot the category cluster and a translucent skin for context
         p.add_mesh(
@@ -411,7 +435,9 @@ def get_mesh_from_model(model: GeoModel, threshold=None):
     """
 
     if model.data is None or model.data.size == 0:
-        raise ValueError("Model data is empty or not computed, no data to show. Use compute model first.")
+        raise ValueError(
+            "Model data is empty or not computed, no data to show. Use compute model first."
+        )
 
     grid = pv.StructuredGrid(model.X, model.Y, model.Z)
 
@@ -440,13 +466,19 @@ def get_voxel_grid_from_model(model, threshold=None):
         The voxel grid representation of the geological model, with discrete values for rock types.
     """
     if model.data is None or model.data.size == 0:
-        raise ValueError("Model data is empty or not computed, no data to show. Use compute model first.")
+        raise ValueError(
+            "Model data is empty or not computed, no data to show. Use compute model first."
+        )
     if not all(res > 1 for res in model.resolution):
-        raise ValueError("Voxel grid requires a model resolution greater than 1 in each dimension.")
+        raise ValueError(
+            "Voxel grid requires a model resolution greater than 1 in each dimension."
+        )
 
     # Create a padded grid with n+1 nodes and node spacing equal to model sample spacing
     dimensions = tuple(x + 1 for x in model.resolution)
-    spacing = tuple((x[1] - x[0]) / (r - 1) for x, r in zip(model.bounds, model.resolution))
+    spacing = tuple(
+        (x[1] - x[0]) / (r - 1) for x, r in zip(model.bounds, model.resolution)
+    )
     # pad origin with a half cell size to center the grid
     origin = tuple(x[0] - cs / 2 for x, cs in zip(model.bounds, spacing))
 
